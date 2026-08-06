6357ec07464770d28e54f09ef7827b1a22fd226c
madinaabdurahmonova558-lgtm <madinaabdurahmonova558@gmail.com>
2026-08-06 13:52:26 +0600

    fas

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:new_app/src/app/my_app.dart';
-import 'package:new_app/src/pages/home/providers/cars_provider.dart';
 import 'package:new_app/src/pages/home/providers/subscribtion_provider.dart'; // ← добавить импорт
 import 'package:new_app/src/pages/pages.dart';
 import 'package:provider/provider.dart';
